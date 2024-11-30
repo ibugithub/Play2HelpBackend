4119da93a1553c719bfdfaac574b73f5f040c172
Md Ibrahim Hossain <outsideworkibrahim@gmail.com>
2024-11-30 19:37:44 +0600

    Add game score system

--- games/views.py
+++ games/views.py
@@ -1,5 +1,3 @@
-from django.shortcuts import render
-from .models import Score
 from .serializers import ScoreSerializer 
 from rest_framework.views import APIView
 from rest_framework import status
@@ -14,11 +12,10 @@ class SubmitScoreView(APIView):
 
   def post(self, request):
     serializer = ScoreSerializer(data=request.data)
+    print('the request.data is:', request.data)
     if serializer.is_valid():
       user = request.user
       print('the user is:', user)
-      score = serializer.validated_data['score']
-      # Score.objects.create(user=user, score=score)
       serializer.save(user=request.user)
       return Response({'message': 'Score submitted successfully'}, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
