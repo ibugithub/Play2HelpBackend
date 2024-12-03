69951e4ca0e33f692e7155a88c646bd98383ea49
Md Ibrahim Hossain <outsideworkibrahim@gmail.com>
2024-12-03 18:28:12 +0600

    Add Reward field into Score Model

--- games/models.py
+++ games/models.py
@@ -8,6 +8,7 @@ class Score(models.Model):
   user = models.ForeignKey(User, on_delete=models.CASCADE)
   game = models.CharField(max_length=100)
   score = models.IntegerField()
+  reward = models.FloatField(default=0.0)
   created_at = models.DateTimeField(auto_now_add=True)
   
   def __str__(self):
